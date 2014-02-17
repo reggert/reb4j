612ab48ca21e9a7fd1375364d7b4a58c1cb50241
Richard W. Eggert II <richard.eggert@gmail.com>
2014-02-17 03:04:10 -0500

    corrected AdoptedProps
    GitHub issue #23 - Migrate unit tests to ScalaCheck

--- src/test/scala/io/github/reggert/reb4j/test/AdoptedProps.scala
+++ src/test/scala/io/github/reggert/reb4j/test/AdoptedProps.scala
@@ -6,7 +6,7 @@ import org.scalatest.junit.JUnitSuite
 import org.scalatest.prop.Checkers
 import org.junit.Test
 
-object AdoptedProps extends JUnitSuite with Checkers 
+class AdoptedProps extends JUnitSuite with Checkers 
 	with ExpressionProperties[Adopted] with AdoptedGenerators 
 {
 	@Test def toPattern : Unit = toPattern(arbitrary[Adopted])
