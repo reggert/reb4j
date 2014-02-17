4d4f19293bcefc20bde3e8698b87ae97765d8832
Richard W. Eggert II <richard.eggert@gmail.com>
2014-02-16 21:26:34 -0500

    adapted Adopted test from reb4s
    GitHub issue #23 - Migrate unit tests to ScalaCheck

---
+++ src/test/scala/io/github/reggert/reb4j/test/AdoptedGenerators.scala
@@ -0,0 +1,21 @@
+package io.github.reggert.reb4j.test
+
+import org.scalacheck.{Arbitrary, Gen}
+import Arbitrary.arbitrary
+import io.github.reggert.reb4j.Adopted
+import java.util.regex.Pattern
+
+
+trait AdoptedGenerators {
+	implicit val arbAdopted = Arbitrary(Gen.sized {size => Gen.choose(1, size) flatMap (genAdopted)})
+	
+	
+	// This generator only generates patterns for quoted strings
+	private def genPattern(size : Int) : Gen[Pattern] = for {
+		s <- Gen.listOfN(size, arbitrary[Char])
+	} yield Pattern.compile(Pattern.quote(s.mkString))
+	
+	def genAdopted(size : Int) : Gen[Adopted] = for {
+		p <- genPattern(size)
+	} yield Adopted.fromPattern(p)
+}
---
+++ src/test/scala/io/github/reggert/reb4j/test/AdoptedProps.scala
@@ -0,0 +1,14 @@
+package io.github.reggert.reb4j.test
+
+import org.scalacheck.{Arbitrary, Properties}
+import Arbitrary.arbitrary
+import io.github.reggert.reb4j.Adopted
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.prop.Checkers
+import org.junit.Test
+
+object AdoptedProps extends JUnitSuite with Checkers 
+	with ExpressionProperties[Adopted] with AdoptedGenerators 
+{
+	@Test def toPattern : Unit = toPattern(arbitrary[Adopted])
+}
